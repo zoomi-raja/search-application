78ed6d5749102a889ef5141c83851d2ce6f9e0b4
zoomi-raja <zoomi.raja@gmail.com>
2020-08-16 13:55:55 +0400

    changed logic for mocking

--- api/test/api.test.js
+++ api/test/api.test.js
@@ -1,30 +1,9 @@
+jest.mock("node-fetch");
 const app = require("../app/app");
 const request = require("supertest");
 //mocking
-const service = require("../app/services/search");
-global.fetch = jest.fn(() => {
-	Promise.resolve({
-		json: () =>
-			Promise.resolve({
-				status: "success",
-				results: 1,
-				data: {
-					entity: "users",
-					result: [
-						{
-							login: "zoomi-raja",
-							id: 27607504,
-							avatar_url:
-								"https://avatars1.githubusercontent.com/u/27607504?v=4",
-							html_url: "https://github.com/zoomi-raja",
-							type: "User",
-						},
-					],
-				},
-			}),
-	});
-});
-//test for listing available git entities
+const fetch = require("node-fetch");
+
 it("should get entities", async (done) => {
 	const appObj = request(app);
 	const res = await appObj.get("/api/entities");
@@ -32,12 +11,46 @@ it("should get entities", async (done) => {
 	expect(res.body.data).toEqual(["users", "issues", "repositories"]);
 	done();
 });
-//get list of results from git (moked response is used)
+
 it("Get results from git", async (done) => {
+	const { Response } = jest.requireActual("node-fetch");
+	fetch.mockReturnValue(
+		Promise.resolve(
+			new Response(
+				JSON.stringify({
+					total_count: 199,
+					incomplete_results: false,
+					items: [
+						{
+							login: "zoomi-raja",
+							id: 27607504,
+							node_id: "MDQ6VXNlcjI3NjA3NTA0",
+							avatar_url:
+								"https://avatars1.githubusercontent.com/u/27607504?v=4",
+							gravatar_id: "",
+							url: "https://api.github.com/users/zoomi-raja",
+							html_url: "https://github.com/zoomi-raja",
+							repos_url: "https://api.github.com/users/zoomi-raja/repos",
+							type: "User",
+							site_admin: false,
+							score: 1,
+						},
+					],
+				})
+			)
+		)
+	);
 	const res = await request(app)
 		.post("/api/search")
-		.send({ text: "umar", entity: "users" });
+		.send({ text: "zamurd", entity: "users" });
 	expect(res.statusCode).toEqual(200);
 	expect(res.body.data).toHaveProperty("result");
 	done();
+});
+
+it("should clear cache", async () => {
+	const appObj = request(app);
+	const res = await appObj.delete("/api/clear-cache");
+	expect(res.statusCode).toEqual(200);
+	expect(res.body.status).toEqual("success");
 });
